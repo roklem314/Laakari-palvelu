25eef0fe19983f522173d7eac74056e78a49264a
JL <lab.galom@outlook.com>
2018-04-06 13:42:03 +0300

    prostgreSql tuodaan kayttoon

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 # tietokanta
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kanta.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///kanta.db"
+    app.config["SQLALCHEMY_ECHO"] = True
+
 
 db = SQLAlchemy(app)
 
@@ -39,4 +46,7 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 # luodaan taulut tietokantaan tarvittaessa
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
